b0e4ee013735d0273da83204ff78736582dd72ec
Aylin <aylin_santacruz@hotmail.com>
2024-11-10 14:10:59 -0500

    feat:Add assistant creation and retrieval functions

---
+++ src/services/assistant.service.ts
@@ -0,0 +1,14 @@
+import { Assistant } from "../models/Assistant";
+import { IAssistant } from '../interface/index.interface';
+
+export const createAssistant = async(data:IAssistant):Promise<IAssistant> =>{
+    if(!data.image){
+        throw new Error('Image is required');
+    }
+
+    return await Assistant.create(data);
+}
+
+export const getAllAssistant = async():Promise<IAssistant[]> =>{
+    return await Assistant.findAll();
+}
