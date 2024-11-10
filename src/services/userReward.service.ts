d724faf1fb69f94dfb5860748aceba24416e2db7
Diana <educ.dana@gmail.com>
2024-11-10 01:00:23 -0500

    feat: add getAvailablePoints services

--- src/services/userReward.service.ts
+++ src/services/userReward.service.ts
@@ -1,23 +1,50 @@
-import { User, UserReward } from "../models";
+import { Op } from "sequelize";
+import { Reward, User, UserReward } from "../models";
 
 interface IUserReward{
     user_id:number,
     reward_id:number,
 }
-
-export const existingRewardServices = async(userId: number, rewardId: number) => {
-    const rewardUser_id = rewardId;
-    if(rewardUser_id!){
-        throw new Error("Reward not found");
+//obtener los puntos disponibles
+export const getAvailablePoints = async (userId: number): Promise<number> => {
+    const user = await User.findByPk(userId);
+    if (!user) {
+        throw new Error('Usuario no encontrado');
     }
-    const existingReward = await UserReward.findOne({
-        where: {
-            user_id: userId,
-            reward_id: rewardId,
-        }
+
+    const totalPoints = user.total_point;
+
+    if (totalPoints === undefined || totalPoints === null) {
+        throw new Error('El usuario no tiene puntos');
+    }
+
+    const userRewards = await UserReward.findAll({
+        where: { user_id: userId }
     });
-    return existingReward
+
+    let pointsSpent = 0;
+
+    if (userRewards.length > 0) {
+        const rewardIds = userRewards.map(ur => ur.reward_id);
+
+        const result = await Reward.findAll({
+            where: {
+                reward_id: rewardIds
+            },
+            attributes: ['required_points']
+        });
+
+        pointsSpent = result.reduce((total, userReward) => {
+            return total + userReward.required_points;
+        }, 0);
+    }
+
+    const availableUserPoints = totalPoints - pointsSpent;
+
+    return availableUserPoints >= 0 ? availableUserPoints : 0;
 }
+    
+
 export const insertRewardUserServices = async(userId: number, rewardId: number,rewardType: string ):Promise<IUserReward> => {
     const user = await User.findByPk(userId);
     if (!user) {
@@ -31,6 +58,7 @@ export const insertRewardUserServices = async(userId: number, rewardId: number,r
     return insertReward;
    
 }
+
 export const allUserRewardsServices = async(userId: number, type:string): Promise<UserReward[]>  => { 
     const user = await User.findByPk(userId);
     if (!user) {
