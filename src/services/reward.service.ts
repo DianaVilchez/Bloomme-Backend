8e65551332064e06e30e940cf23ae5ff40391b8f
Diana <educ.dana@gmail.com>
2024-11-10 01:05:02 -0500

    fix: change to reward service

--- src/services/reward.service.ts
+++ src/services/reward.service.ts
@@ -1,5 +1,6 @@
 import { Op } from "sequelize";
 import { Reward, User, UserReward } from "../models";
+import { getAvailablePoints } from "./userReward.service";
 
 export const allRewardService = async (): Promise<Reward[]> => {
     const allReward = await Reward.findAll();
@@ -29,25 +30,32 @@ export const getUnlockedRewardsServices = async (userId: number): Promise<Reward
     if (!user) {
         throw new Error("User not found");
     }
-    const userPoints = user.total_point;
+    const userPoints = await getAvailablePoints(userId);
+    const unlockedRewardIds = await UserReward.findAll({
+        where: { user_id: userId },
+        attributes: ['reward_id'],
+    }).then(rewards => rewards.map(reward => reward.reward_id));
+   
+    console.log("userPoints," ,userPoints)
     const unlockedRewards = await Reward.findAll({
         where:{
             required_points:{
                 [Op.lte] :  userPoints
-            }
+            },
+            reward_id: { [Op.notIn]: unlockedRewardIds }
         }
     })
     return unlockedRewards;
 }
 
-export const getRequiredPointsServices =  async (RewardId: number): Promise<number> =>{
-    const rewardPoint = await Reward.findOne({
-        where: { reward_id: RewardId},
-        attributes: ['required_points']
-    });
-    if(!rewardPoint){
-        throw new Error("Reward not found");
-    }
-    return rewardPoint.required_points;
-    ;
-}
+// export const getRequiredPointsServices =  async (RewardId: number): Promise<number> =>{
+//     const rewardPoint = await Reward.findOne({
+//         where: { reward_id: RewardId},
+//         attributes: ['required_points']
+//     });
+//     if(!rewardPoint){
+//         throw new Error("Reward not found");
+//     }
+//     return rewardPoint.required_points;
+//     ;
+// }
