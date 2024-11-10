ab773536ae1f607306a20d823cd6ccfff36fcaf6
Diana <educ.dana@gmail.com>
2024-11-10 15:22:53 -0500

    refactor: update route in endpoint select

--- src/routes/userReward.ts
+++ src/routes/userReward.ts
@@ -4,7 +4,7 @@ import { isAuthenticated } from '../middleware/auth.middleware';
 
 const userRewardRouter : Router = express.Router();
 
-userRewardRouter.get('/user-reward/:reward_id/:reward_type',isAuthenticated,selectReward);
+userRewardRouter.get('/user-reward/select',isAuthenticated,selectReward);
 userRewardRouter.get('/user-reward/:user_id/:reward_type',allUserRewards);
 
 export{userRewardRouter}
