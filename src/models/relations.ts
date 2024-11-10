12a52d83db2d641e88c66fce2a6c1073032fb6e8
Aylin <aylin_santacruz@hotmail.com>
2024-11-10 14:26:14 -0500

    feat:Define model relationships for User, Assistant, and related entities

--- src/models/relations.ts
+++ src/models/relations.ts
@@ -1,3 +1,4 @@
+import { Assistant } from "./Assistant";
 import { Module } from "./Module";
 import { Option } from "./Option";
 import { Path } from "./Path";
@@ -27,3 +28,5 @@ Module.belongsTo(Path,{foreignKey: 'path_id'});
 Question.hasMany(Option, { foreignKey: 'question_id' , as: 'Options' });
 Option.belongsTo(Question, { foreignKey: 'question_id' });
 
+Assistant.hasMany(User,{foreignKey:'assistant_id',as:'Users'});
+User.belongsTo(Assistant,{foreignKey:'assistant_id',as:'Assistant'});
