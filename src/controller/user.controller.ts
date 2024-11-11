3cd124a85652f19fc9995ded9bb2a2e5ce233d3c
Diana <educ.dana@gmail.com>
2024-11-11 11:25:23 -0500

    feat: add getUserCurrentRewardController controller to user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -55,3 +55,29 @@ export const getUserProfileController = async (req: Request, res: Response) => {
         }
     }
 }
+
+
+export const getUserCurrentRewardController = async (req: Request, res: Response) => {
+    try {
+        const user_id = req.user_id;
+        const type = req.params.type;
+        if (!user_id || isNaN(user_id)) {
+            res.status(400).json({ error: 'The user ID is invalid.' });
+            return;
+        }
+        const userReward = await getUserDetailsById(user_id);
+        if(type==='avatar'){
+            res.status(200).json(userReward.current_avatar)
+        }
+        if(type==='background'){
+            res.status(200).json(userReward.current_background)
+        }
+        
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(404).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: 'An unexpected error has occurred' });
+        }
+    }
+}
