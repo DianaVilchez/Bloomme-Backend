21974316ce78de2dc2ad0cce0be92da33e8555d1
Antone Gonzales <es.angelika@gmail.com>
2024-11-11 04:18:08 -0500

    feat: add function to retrieve exercises by emotion from the database

--- src/services/exercise.service.ts
+++ src/services/exercise.service.ts
@@ -1,4 +1,6 @@
-import { Exercises } from "../models/Exercises";
+import { Exercises } from "../models";
+
+
 
 export const createExerciseService = async (name: string, emotion_id: number) => {
     try {
@@ -12,3 +14,21 @@ export const createExerciseService = async (name: string, emotion_id: number) =>
         throw new Error("Error creating exercise. ");
     }
 };
+
+export const getExercisesByEmotion = async (emotion_id: number): Promise<Exercises[]> => {
+    try {
+      const exercises = await Exercises.findAll({
+        where: { emotion_id }, 
+      });
+      
+      if (exercises.length === 0) {
+        throw new Error('No exercises found for this emotion');
+      }
+      console.log("Resultado de Exercises en getExercisesByEmotion",exercises);
+      
+      return exercises;
+    } catch (error) {
+      console.error('Error fetching exercises by emotion:', error);
+      throw new Error('Error fetching exercises from the database');
+    }
+  };
