4210bb5a68e07e8ea69ad4d1d30bf0dca9576bcd
Aylin <aylin_santacruz@hotmail.com>
2024-11-10 23:19:57 -0500

    feat:add controller and router to score

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
-import { getAllUser, updateUserController } from '../controller/user.controller';
+import { getAllUser, getUserScoreController, updateUserController } from '../controller/user.controller';
+import { isAuthenticated } from '../middleware/auth.middleware';
 
 const userRouter: Router = express.Router();
 
 userRouter.get('/user',getAllUser);
+userRouter.get('/user/score',isAuthenticated,getUserScoreController)
 userRouter.put('/user/:user_id',updateUserController)
 
 export {userRouter}
