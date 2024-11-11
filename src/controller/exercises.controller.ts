c276097fb1d711eb9ec5c0a75343745f9370b75d
Antone Gonzales <es.angelika@gmail.com>
2024-11-11 04:15:00 -0500

    feat: add controllers to generate exercise text and fetch exercises by emotion

--- src/controller/exercises.controller.ts
+++ src/controller/exercises.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { createExerciseService } from "../services/exercise.service";
+import { createExerciseService, getExercisesByEmotion } from "../services/exercise.service";
+import { generateEmotionExerciseText } from "../services/gemini.service";
 
 export const createExerciseController = async (req: Request, res: Response) => {
     const { name, emotion_id } = req.body;
@@ -16,3 +17,33 @@ export const createExerciseController = async (req: Request, res: Response) => {
         res.status(500).json({ message: "An error occurred while creating the exercise" });
     }
 };
+
+export const generateExerciseTextController = async (req: Request, res: Response) => {
+    const { emotion_id, exercises_id } = req.query;
+  
+    // if (!emotion_id || !exercises_id) {
+    //   return res.status(400).json({ error: 'Emotion ID and Exercise ID are required' });
+    // }
+  
+    try {
+      const generatedText = await generateEmotionExerciseText(Number(emotion_id), Number(exercises_id));
+  
+      res.json({ text: generatedText });
+    } catch (error) {
+      console.error('Error generating the exercise text:', error);
+      res.status(500).json({ error: 'Error generating the exercise text' });
+    }
+};
+
+export const getExercisesByEmotionController = async (req: Request, res: Response) => {
+    const { emotion_id } = req.params;  // Obtener el emotion_id desde los parámetros de la ruta
+    console.log('Emotion ID:', emotion_id)
+    try {
+      const exercises = await getExercisesByEmotion(Number(emotion_id));  // Obtener ejercicios por emoción
+      res.json(exercises);  // Retornar los ejercicios en formato JSON
+    } catch (error) {
+      console.error('Error fetching exercises by emotion:', error);
+      res.status(500).json({ error: 'Error fetching exercises from the database' });
+    }
+  };
+  
