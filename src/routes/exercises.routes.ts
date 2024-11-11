f4856b1fd82cf8cb0b2c7a83066dceb283ed1626
Antone Gonzales <es.angelika@gmail.com>
2024-11-11 04:17:18 -0500

    feat: add route to fetch all exercises

--- src/routes/exercises.routes.ts
+++ src/routes/exercises.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createExerciseController } from "../controller/exercises.controller";
-createExerciseController
+import { createExerciseController, generateExerciseTextController, getExercisesByEmotionController } from "../controller/exercises.controller";
+import { isAuthenticated } from "../middleware/auth.middleware";
 
 const exercisesRouter = express.Router();
 
 exercisesRouter.post("/exercises", createExerciseController);
-
+exercisesRouter.get('/generate-emotion-exercise-text',isAuthenticated, generateExerciseTextController);
+exercisesRouter.get('/exercises/:emotion_id',isAuthenticated, getExercisesByEmotionController);
 export { exercisesRouter };
