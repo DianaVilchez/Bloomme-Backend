f91b4006c83976c7d4841d68b898ad0ccbb25777
Diana <educ.dana@gmail.com>
2024-11-11 03:10:05 -0500

    feat: add new userRewardRouter

--- src/routes/userReward.ts
+++ src/routes/userReward.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { allUserRewards, selectReward } from '../controller/userReward.controller';
+import { allUserRewards, pointsAvailable, selectReward } from '../controller/userReward.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
 
 const userRewardRouter : Router = express.Router();
 
 userRewardRouter.get('/user-reward/select',isAuthenticated,selectReward);
 userRewardRouter.get('/user-reward/:reward_type',isAuthenticated, allUserRewards);
+userRewardRouter.get('/points-reward',isAuthenticated, pointsAvailable);
 
 export{userRewardRouter}
