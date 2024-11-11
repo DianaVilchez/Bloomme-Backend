2f85b7137eca2ae612db871ddb9dc0f45a39aac5
Diana <educ.dana@gmail.com>
2024-11-11 11:25:38 -0500

    feat: add route to userRouter

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { getAllUser, getUserProfileController, updateUserController } from '../controller/user.controller';
+import { getAllUser, getUserCurrentRewardController, getUserProfileController, updateUserController } from '../controller/user.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
 
 const userRouter: Router = express.Router();
@@ -7,5 +7,5 @@ const userRouter: Router = express.Router();
 userRouter.get('/user',getAllUser);
 userRouter.get('/profile',isAuthenticated,getUserProfileController)
 userRouter.put('/user/:user_id',updateUserController)
-
+userRouter.get('/profile-reward/:type',isAuthenticated,getUserCurrentRewardController)
 export {userRouter}
