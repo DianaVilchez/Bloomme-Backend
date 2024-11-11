d4343ae695051c397944996264213132c69a3251
Aylin <aylin_santacruz@hotmail.com>
2024-11-11 01:35:34 -0500

    feat:Add profile

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express';
-import { getAllUser, getUserScoreController, updateUserController } from '../controller/user.controller';
+import { getAllUser, getUserProfileController, updateUserController } from '../controller/user.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
 
 const userRouter: Router = express.Router();
 
 userRouter.get('/user',getAllUser);
-userRouter.get('/user/score',isAuthenticated,getUserScoreController)
+userRouter.get('/profile',isAuthenticated,getUserProfileController)
 userRouter.put('/user/:user_id',updateUserController)
 
 export {userRouter}
