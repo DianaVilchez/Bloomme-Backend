402bba004a7b1bdf737014f677e3f9e602c123a9
Diana <educ.dana@gmail.com>
2024-11-13 09:42:08 -0500

    chore: General code adjustments , including updated messages to Routes

--- src/routes/path.routes.ts
+++ src/routes/path.routes.ts
@@ -1,7 +1,6 @@
 import express, { Router } from 'express';
 import { getAllPathController, postPathController } from '../controller/path.controller';
 import { getModulesByPathController } from '../controller/module.controller';
-// import { isAuthenticated } from '../middleware/auth.middleware';
 
 const pathRouter: Router = express.Router();
 
